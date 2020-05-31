0841cf4f7f10863b3e04d10a15fce62d7a8670b4
Eliú Díaz <eedb_abcd_1234567890@ymail.com>
2020-05-31 11:02:08 -0500

    Section 9, Chapter 219

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
   EditProductScreen({Key key}) : super(key: key);
 
   @override
@@ -8,14 +9,47 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
       ),
-      body: Center(
-        child: Text('Hello!'),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  textInputAction: TextInputAction.next,
+                  // onFieldSubmitted gets a value from the TextFormField, we instead use '_', to tell it we don't need it
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFocusNode,
+                ),
+              ],
+            ),
+          ),
+          //   child: ListView(
+          //     children: <Widget>[
+          //       TextFormField(
+          //         decoration: InputDecoration(labelText: 'Title'),
+          //         textInputAction: TextInputAction.next,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+        ),
       ),
     );
   }
--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
+
+import '../screens/edit_product_screen.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 
@@ -16,7 +18,9 @@ class UserProductScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           )
         ],
       ),
